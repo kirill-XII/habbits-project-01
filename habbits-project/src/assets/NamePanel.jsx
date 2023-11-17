4ffc59872223ea0db822e1b18ff507d5945e279c
kirill <116079986+kirill-XII@users.noreply.github.com>
2023-11-17 09:50:56 +0300

    NamePanel component and module.css(frome Gpt)

---
+++ habbits-project/src/assets/NamePanel.jsx
@@ -0,0 +1,13 @@
+import s from './NamePanel.module.css'
+const NamePanel = () => {
+	return (
+		<>
+			<div className={s.panel}>
+				<h1>Habbits</h1>
+				<button>ENG</button>
+			</div>
+		</>
+	)
+}
+
+export default NamePanel
